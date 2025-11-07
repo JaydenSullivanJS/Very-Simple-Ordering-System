bfe66e32b0a4cffc42a10aced04426ebfde1ef92
JaydenSullivanJS <jayden.sullivan.tech@gmail.com>
2025-11-07 10:37:45 +0000

    Added the functions in main.c
    I added functions to the main.c so it works properly. I also added some text to help the user know that this is just a very basic program, so people can learn C.

--- main.c
+++ main.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+#include "order_header_file.h"
 
+int main() {
+    printf("Welcome to the very simple ordering checking system this was made for people that want to learn c and want to get better :)\n");
+    printf("-------------------\n");
+    addparcel();
+    checkstatus();
+}
